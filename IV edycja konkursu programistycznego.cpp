0c912c1bb03d72de3a281b7713a62a77908f5d34
Samuel Rus <148883293+ruus77@users.noreply.github.com>
2024-07-20 14:13:03 +0200

    Add files via upload

---
+++ IV edycja konkursu programistycznego.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+
+int main(int argc, char *argv[]) {
+    int a, b, c, d, e;
+    cin>>a;
+    cin>>b;
+    cin>>c;
+    cin>>d;
+    cin>>e;
+    if( a > 0 &&
+        b > 0 &&
+        c > 0 &&
+        d > 0 &&
+        e > 0) {
+        int wynik = 5 + a + b + c + d+ e;
+        cout<<wynik;
+    }
+}
