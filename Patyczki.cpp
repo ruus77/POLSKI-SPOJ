b7b33d41f48a6b170d3375e08c80ed44788604d0
Samuel Rus <148883293+ruus77@users.noreply.github.com>
2024-07-21 00:32:03 +0200

    Add files via upload

---
+++ Patyczki.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+int main() {
+    int n;
+    cin >> n;
+
+    if (n > 0 && n <= 1024) {
+        vector<int> patyczki(n);
+        for (int i = 0; i < n; ++i) {
+            cin >> patyczki[i];
+        }
+        int dlugosci[257] = {0};
+        for (int i = 0; i < n; ++i) {
+            if (patyczki[i] > 0 && patyczki[i] <= 256) {
+                dlugosci[patyczki[i]]++;
+            }
+        }
+
+        int liczbaTrojkatow = 0;
+        for (int i = 1; i <= 256; ++i) {
+            liczbaTrojkatow += dlugosci[i] / 3;
+        }
+
+        cout << liczbaTrojkatow << endl;
+    }
+
+    return 0;
+}
